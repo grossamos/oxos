bd36764b3a4362f4671999bb0f39e959acb18e42
grossamos <email@amosgross.com>
2023-01-12 20:29:14 +0100

    unknown progress

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-#![no_std]
-#![no_main]
+#![feature(lang_items)]
 #![feature(core_intrinsics)]
 #![feature(stdsimd)]
+#![no_main]
+#![no_std]
+
+extern crate compiler_builtins;
 
 use core::panic::PanicInfo;
 
+use framebuffer::Framebuffer;
 use uart::{uart_init, uart_send};
 
 mod uart;
 mod utils;
 mod framebuffer;
+mod gpio;
 
 #[no_mangle]
 pub extern fn kernel_main() {
@@ -18,9 +23,13 @@ pub extern fn kernel_main() {
     uart_init();
     uart_send(message);
 
+    let fb = Framebuffer::new();
+    fb.draw_pixel(0, 0);
+
     loop {
     }
 }
+
   
 #[panic_handler]
 #[no_mangle]
@@ -28,3 +37,5 @@ fn panic(_panic: &PanicInfo<'_>) -> ! {
    loop {}
 } 
 
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
