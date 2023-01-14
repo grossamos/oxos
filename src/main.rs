4df78e8eb593399c44a5ab12410663a0df2faba4
grossamos <email@amosgross.com>
2023-01-14 19:24:01 +0100

    fixed older issues

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-#![no_std]
-#![no_main]
-#![feature(core_intrinsics)]
+#![feature(asm_const)]
 #![feature(stdsimd)]
+#![no_main]
+#![no_std]
 
 use core::panic::PanicInfo;
-
+use core::arch::global_asm;
 use uart::{uart_init, uart_send};
 
 mod uart;
@@ -21,6 +21,8 @@ pub extern fn kernel_main() {
     loop {
     }
 }
+
+global_asm!(include_str!("boot.s"));
   
 #[panic_handler]
 #[no_mangle]
